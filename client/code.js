5bcd5724f8f7a42a5976d72320a46b824efdee89
Alberto Diaz <alberto.diaz05@gmail.com>
2021-07-13 23:16:39 -0400

    Se agrega busqueda de entidades, se muestran codigos de anotaciones

--- client/code.js
+++ client/code.js
@@ -1,32 +1,51 @@
 async function getEntities(req) {
-    const response = await fetch(new Request(`/api/entities/${req}`));
+    var myRequest = new Request(`/api/entities/${req}`);
+    const response = await fetch(myRequest);
     const data = await response.json();
    
     return data;
 }
 
 function fillEntities(req) {
+   
     getEntities(req).then(data => {
+        //console.log(data.entities);
         const ulEntities = document.getElementById("entities");
-        const ulLabels = document.getElementById("labels");
-        console.log('req: '+req);
+        const ulLabels = document.getElementById("claims");
+        
         for(var attributename in data.entities[req]){
-            console.log('data 2: '+data.entities[req]);
             const liEntity = document.createElement("li");
             const text = document.createTextNode(attributename+": "+data.entities[req][attributename]);
             liEntity.appendChild(text);
-            ulEntities.appendChild(liEntity);
+            //ulEntities.appendChild(liEntity);
+            document.getElementById('country-code').innerHTML=req;
 
-            if (attributename == 'labels'){
-                const labelsArray =  data.entities[req][attributename];
-                for(var label in labelsArray){
+            if(attributename == 'labels'){
+                const labelsArray =  data.entities[req][attributename]
+                for(let label in labelsArray){
+                    if(label =='es'){
+                        const countryName = labelsArray[label]['value'];
+                        document.getElementById('country-name').innerHTML=countryName;
+                        console.log(countryName);
+                    }
+                }
+
+
+            }
+
+            if (attributename == 'claims'){
+
+                const claimsArray =  data.entities[req][attributename];
+                for(var claim in claimsArray){
+
+                    console.log(JSON.stringify(claim));
+
                     const liLabel = document.createElement("li");
-                    const textLabel = document.createTextNode(label+": "+labelsArray[label]['value']);
+                    const textLabel = document.createTextNode(claim);
                     liLabel.appendChild(textLabel);
-                    ulLabels.appendChild(liLabel);        
+                    ulLabels.appendChild(liLabel);
                 }
             }
         }
-        console.log(4);
     })
 }
